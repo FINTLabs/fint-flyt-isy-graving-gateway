8a3c5372ce9f38b30a4c0a4db10b390357ac86d3
Jan Ove Kongshaug <janovekongshaug@Jan-sin-MacBook-Pro.local>
2025-08-07 10:11:15 +0200

    Initialiser repo med Kotlin

---
+++ src/main/kotlin/no/fintlabs/isy_graving_instance_gateway/IsyGravingInstanceGatewayApplication.kt
@@ -0,0 +1,11 @@
+package no.fintlabs.isy_graving_instance_gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IsyGravingInstanceGatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<IsyGravingInstanceGatewayApplication>(*args)
+}
