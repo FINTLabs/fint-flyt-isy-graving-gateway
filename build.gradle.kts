9eb9fa9eb86801a4de3335ffa750a20ff510452d
Egil Ballestad <egil.ballestad@vigoiks.no>
2026-09-21 15:00:55 +0200

    bump flyt-gateway-starter version til 4.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ plugins {
 group = "no.novari"
 version = "0.0.1-SNAPSHOT"
 
+var springdocOpenApiVersion = "2.8.17"
+
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(25))
@@ -40,11 +42,11 @@ dependencies {
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-    implementation("no.novari:flyt-gateway-starter:4.0.0")
+    implementation("no.novari:flyt-gateway-starter:4.1.0")
     implementation("no.novari:flyt-cache:3.0.0")
 
     // Springdoc 3.x targets Spring Boot 4.x; keep 2.x for Boot 3.5.x compatibility.
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.17")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocOpenApiVersion")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
